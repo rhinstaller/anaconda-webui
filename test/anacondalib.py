db9cf0cb5ffc13f4c060f732fca93977d31db379
Zdenek Veleba <zveleba@redhat.com>
2023-11-23 15:06:50 +0100

    test: make wipefs in resetStorage more universal.

--- test/anacondalib.py
+++ test/anacondalib.py
@@ -70,7 +70,7 @@ class VirtInstallMachineCase(MachineCase):
         b = self.browser
         s = Storage(b, m)
 
-        m.execute("wipefs --all /dev/vda")
+        m.execute("find /dev -regex '/dev/[v|s]d.' -exec wipefs --all {} \;")
         s.dbus_reset_partitioning()
         s.dbus_reset_selected_disks()
         # CLEAR_PARTITIONS_DEFAULT = -1
