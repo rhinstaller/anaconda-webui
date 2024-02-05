096fb7a58b6962ee273e1d5e78722bae1603d6ba
Katerina Koukiou <kkoukiou@redhat.com>
2024-02-05 21:46:17 +0100

    tests: wait longer than default timeout for mount-point map page to appear

--- test/helpers/storage.py
+++ test/helpers/storage.py
@@ -381,7 +381,7 @@ class StorageMountPointMapping(StorageDBus, StorageDestination):
         if format_type:
             self.check_mountpoint_row_format_type(row, format_type)
 
-    def select_mountpoint(self, disks):
+    def select_mountpoint(self, disks, encrypted=False):
         self.browser.wait(lambda : self.disks_loaded(disks))
 
         for disk in disks:
@@ -393,6 +393,12 @@ class StorageMountPointMapping(StorageDBus, StorageDestination):
 
         i = Installer(self.browser, self.machine)
         i.next(next_page=i.steps.CUSTOM_MOUNT_POINT)
+
+        with self.browser.wait_timeout(30):
+            if not encrypted:
+                self.browser.wait_visible("#mount-point-mapping-table")
+            else:
+                self.browser.wait_not_present("#unlock-device-dialog")
 
     def select_mountpoint_row_mountpoint(self, row,  mountpoint):
         self.browser.set_input_text(f"{self.table_row(row)} td[data-label='Mount point'] input", mountpoint)
