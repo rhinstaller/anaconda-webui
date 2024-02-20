56f94e4be5c6c8ffdc97ece343547ec40b5a98de
Adam Kaňkovský <akankovs@redhat.com>
2024-02-20 10:57:06 +0100

    Introduce dark mode support

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
-
+import "cockpit-dark-theme";
 import "../pkg/lib/patternfly/patternfly-5-cockpit.scss";
 
 import React from "react";
