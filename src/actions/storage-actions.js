rhinstaller/anaconda-webui

commit f66f8f4befb6c095f544b6c7e579078c1b8891c0

storage: fix crash when device path is not existing
This happens when there are deleted backing files from loop devices.

Ex:
$ cat /sys/block/loop0/loop/backing_file
/home/vtrefny/disk.img2 (deleted)

Resolves: rhbz#2308278

src/actions/storage-actions.js
@@ -71,12 +71,20 @@ export const getDevicesAction = () => {
 
                     // FIXME: https://github.com/storaged-project/blivet/issues/1258
                     // This is a workaround for the issue with missing partition label human readable strings in blivet
-                    const partName = await cockpit.script(`udevadm info --query=property --name=${devData.path.v} | grep PARTNAME= | cut -d= -f2`);
-                    const fsLabel = await cockpit.script(`udevadm info --query=property --name=${devData.path.v} | grep ID_FS_LABEL= | cut -d= -f2`);
-                    devData.misc = {
-                        fsLabel: fsLabel.trim(),
-                        partName: partName.trim(),
-                    };
+                    try {
+                        const partName = await cockpit.script(`udevadm info --query=property --name=${devData.path.v} | grep PARTNAME= | cut -d= -f2`);
+                        const fsLabel = await cockpit.script(`udevadm info --query=property --name=${devData.path.v} | grep ID_FS_LABEL= | cut -d= -f2`);
+                        devData.misc = {
+                            fsLabel: fsLabel.trim(),
+                            partName: partName.trim(),
+                        };
+                    } catch (e) {
+                        console.warning("Failed to get partition label", e);
+                        devData.misc = {
+                            fsLabel: "",
+                            partName: "",
+                        };
+                    }
 
                     deviceData[device] = devData;
                 } catch (error) {

commit 39620475d01c73b33e3b4c4a05f44157e251fbbe

storage: use `console.warn` not `console.warning` to fix crash
f66f8f4 mistakenly tried to log a warning with `console.warning`,
which doesn't exist. It's `console.warn`. This means we still
crash on the path where we crashed before, now with an error
about `console.warning` being undefined.

Signed-off-by: Adam Williamson <awilliam@redhat.com>

src/actions/storage-actions.js
@@ -79,7 +79,7 @@ export const getDevicesAction = () => {
                             partName: partName.trim(),
                         };
                     } catch (e) {
-                        console.warning("Failed to get partition label", e);
+                        console.warn("Failed to get partition label", e);
                         devData.misc = {
                             fsLabel: "",
                             partName: "",