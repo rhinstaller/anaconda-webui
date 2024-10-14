6859dd06e93c5aac9ba397b5c14b8d5c402d4dd8
Katerina Koukiou <k.koukiou@gmail.com>
2024-10-14 11:01:51 +0200

    Update strings based on latest mockups

--- src/components/storage/HelpAutopartOptions.jsx
+++ src/components/storage/HelpAutopartOptions.jsx
@@ -4,8 +4,8 @@ const _ = cockpit.gettext;
 
 export const helpEraseAll = _("Remove all partitions on the selected devices, including existing operating systems. Make sure you have backed up your data.");
 
-export const helpUseFreeSpace = _("Keep current disk layout and only install into available space.");
+export const helpUseFreeSpace = _("Keep current disk layout and use available space, to dual-boot with another OS.");
 
-export const helpMountPointMapping = _("Manually select storage locations for installation.");
+export const helpMountPointMapping = _("Assign partitions to mount points. Useful for pre-configured custom layouts.");
 
 export const helpConfiguredStorage = _("Storage is based on the configuration from 'Modify storage'.");
