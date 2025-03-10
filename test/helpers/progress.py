4c4fa2f6907118f034ca9ff446e9e5e1cc16bad2
Katerina Koukiou <k.koukiou@gmail.com>
2025-03-10 15:01:10 +0100

    tests: e2e: when waiting for the reboot button to appear check for possible errors
    This will make tests that failed exit earlier.

--- test/helpers/progress.py
+++ test/helpers/progress.py
@@ -32,10 +32,15 @@ class Progress():
     @log_step(snapshot_after=True)
     def wait_done(self, timeout=1200):
         delay = 30
-        wait(lambda: self.browser.is_present(self._reboot_selector), delay=delay, tries=timeout / delay)
-        self.browser.wait_visible(self._reboot_selector)
-        if self.browser.is_present('.pf-v5-c-alert.pf-m-danger'):
+        wait(
+            lambda: self.browser.is_present(self._reboot_selector) or self.browser.is_present("#critical-error-bz-report-modal"),
+            delay=delay,
+            tries=timeout / delay
+        )
+        if self.browser.is_present('#critical-error-bz-report-modal'):
             raise AssertionError('Error during installation')
+
+        self.browser.wait_visible(self._reboot_selector)
 
     @log_step()
     def reboot(self):
