rhinstaller/anaconda-webui

commit dccf67e827beaf21616daf498b445437eca45981

Makefile: always download the gated ISO otherwise testlib will complain
FileNotFoundError: Can't run tests without a prepared image; use test/image-prepare

Makefile
@@ -218,11 +218,8 @@ images: bots
 	bots/image-download debian-stable ubuntu-stable fedora-41 fedora-42 fedora-rawhide
 	# Downoad ISO images: if a compose if specified download from
 	# the compose otherwise download the ISO from Cockpit image server
-	if [ -n "$(TEST_COMPOSE)" ]; then \
-		test/download-iso "$(TEST_OS)" "$(TEST_COMPOSE)" "$(RELEASE)"; \
-	else \
-		bots/image-download "$(TEST_OS)"; \
-	fi
+	[ -n "$(TEST_COMPOSE)" ] && test/download-iso "$(TEST_OS)" "$(TEST_COMPOSE)" "$(RELEASE)"
+	bots/image-download "$(TEST_OS)";
 
 $(UPDATES_IMG): prepare-test-deps
 	test/prepare-updates-img

commit c356706d6d99f2ec9aa25720f1ebec4c83a34750

tests: adjust `make images` target to work even if test compose is not set
This regressed in dccf67e827beaf21616daf498b445437eca45981.

Makefile
@@ -218,8 +218,10 @@ images: bots
 	bots/image-download debian-stable ubuntu-stable fedora-41 fedora-42 fedora-rawhide
 	# Downoad ISO images: if a compose if specified download from
 	# the compose otherwise download the ISO from Cockpit image server
-	[ -n "$(TEST_COMPOSE)" ] && test/download-iso "$(TEST_OS)" "$(TEST_COMPOSE)" "$(RELEASE)"
-	bots/image-download "$(TEST_OS)";
+	@if [ -n "$(TEST_COMPOSE)" ]; then \
+		bots/image-download "$(TEST_OS)" "$(TEST_COMPOSE)" "$(RELEASE)"; \
+	fi
+	bots/image-download "$(TEST_OS)"
 
 $(UPDATES_IMG): prepare-test-deps
 	test/prepare-updates-img