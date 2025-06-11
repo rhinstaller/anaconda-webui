7f4c1d18d5cf18457fb9fd473bcaefb93effb2e9
Katerina Koukiou <k.koukiou@gmail.com>
2025-06-11 08:33:53 +0200

    logging: log debug messages to /tmp/anaconda-webui.log file
    Resolves: INSTALLER-4169

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -15,9 +15,31 @@
  * along with This program; If not, see <http://www.gnu.org/licenses/>.
  */
 
-// see https://github.com/cockpit-project/cockpit/blob/main/HACKING.md#debug-logging-in-javascript-console
-export function debug (...args) {
-    if (window.debugging === "all" || window.debugging?.includes("anaconda")) {
-        console.debug("anaconda", ...args);
+import cockpit from "cockpit";
+
+const LOG_FILE = "/tmp/anaconda-webui.log";
+
+class Logger {
+    constructor () {
+        this.logger = cockpit.file(LOG_FILE);
+    }
+
+    _write (level, args) {
+        const timestamp = new Date().toISOString();
+        const message = `${timestamp} [${level}] ${args.join(" ")}\n`;
+
+        this.logger.modify(oldContent => oldContent + message);
+    }
+
+    debug (...args) {
+        // see https://github.com/cockpit-project/cockpit/blob/main/HACKING.md#debug-logging-in-javascript-console
+        if (window.debugging === "all" || window.debugging?.includes("anaconda")) {
+            console.debug("anaconda", ...args);
+        }
+
+        this._write("DEBUG", args);
     }
 }
+
+const logger = new Logger();
+export const debug = logger.debug.bind(logger);
