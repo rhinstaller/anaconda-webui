444d2d2343026ebceb53312ce1cc7e096a653f6d
Katerina Koukiou <k.koukiou@gmail.com>
2025-07-25 08:51:29 +0200

    systemd: run cockpit-bridge with coproc to fix SELinux denials in Live ISOs
    Fixes SELinux denials from cockpit-bridge when running anaconda-webui in
    Live ISOs. The issue was that cockpit-ws was running in cockpit_ws_t
    context instead of unconfined_t, causing permission issues.
    
    Suggested by cockpit maintainer pitti.

---
+++ src/scripts/cockpit-coproc-wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+# This script implements the coproc approach for the systemd service
+# to avoid SELinux denials while keeping the service manageable with systemctl
+WEBUI_ADDRESS=$1
+
+# Start cockpit-bridge in unconfined context via su using coproc
+coproc BRIDGE { cockpit-bridge; }
+
+# Start cockpit-ws connected to the coproc
+exec /usr/libexec/cockpit-ws -p 80 -a "$WEBUI_ADDRESS" --no-tls --local-session=- <&${BRIDGE[0]} >&${BRIDGE[1]}
