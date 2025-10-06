f6ae4360074f211b0d52c09a2b19153f3e554dee
Radek Vykydal <rvykydal@redhat.com>
2025-10-06 13:55:29 +0200

    Add test for simulating wireless devices and creting AP with hostapd
    NOTE: treid also to use nmcli to check but it was not quite working
    
    - f"nmcli -t -f SSID device wifi list ifname {device}" does not show
      anything
    - if it does it switches wlan0 and wlan1

--- test/helpers/network.py
+++ test/helpers/network.py
@@ -15,6 +15,7 @@
 
 import os
 import sys
+from testlib import wait  # pylint: disable=import-error
 
 HELPERS_DIR = os.path.dirname(__file__)
 sys.path.append(HELPERS_DIR)
@@ -23,6 +24,7 @@ NETWORK_SERVICE = "org.fedoraproject.Anaconda.Modules.Network"
 NETWORK_INTERFACE = NETWORK_SERVICE
 NETWORK_OBJECT_PATH = "/org/fedoraproject/Anaconda/Modules/Network"
 
+HOSTAPD_PIDFILE = "/var/tmp/webui_hostapd_pid"
 
 class NetworkDBus():
     def __init__(self, machine):
@@ -47,3 +49,63 @@ class NetworkDBus():
 
     def dbus_reset_hostname(self):
         self.dbus_set_hostname("''")
+
+
+# FIXME move to common/netlib.py when more mature
+class Network():
+    def __init__(self, browser, machine):
+        self.browser = browser
+        self.machine = machine
+
+    def _wait_for_wifi_ifaces(self, number):
+        for idx in range(number):
+            wait(lambda: self.machine.execute(f"find /sys/class/net/wlan{idx}/address 2>/dev/null || true"))
+
+    def create_wifi_devices(self, number=1):
+        self.machine.execute(f"modprobe mac80211_hwsim radios={number}")
+        self._wait_for_wifi_ifaces(number)
+        self.machine.execute("udevadm trigger; udevadm settle")
+        self._wait_for_wifi_ifaces(number)
+
+    def remove_wifi_devices(self):
+        self.machine.execute(f"modprobe -r mac80211_hwsim")
+
+    def create_ap(
+        self,
+        ssid_name,
+        interface="wlan0",
+        secret="secret123",
+    ):
+
+        if os.path.exists(HOSTAPD_PIDFILE):
+            raise RuntimeError("Can't run hostapd, found running daemon pid file {HOSTAPD_PIDFILE}")
+
+        conf_file = self.machine.execute("mktemp /var/tmp/hostapd.XXXXX.conf").strip()
+        self.machine.write(conf_file, f'''interface={interface}
+driver=nl80211
+ssid={ssid_name}
+channel=6
+hw_mode=g
+wpa=2
+wpa_key_mgmt=WPA-PSK
+rsn_pairwise=CCMP
+wpa_passphrase={secret}
+''')
+        self.machine.execute(f"hostapd -P {HOSTAPD_PIDFILE} -dd -B {conf_file}")
+        self._wait_for_ssids(interface, [ssid_name])
+
+    def remove_ap(self):
+        self.machine.execute(f"kill -TERM `cat {HOSTAPD_PIDFILE}`")
+
+    def get_ssids(self, iface):
+        ssid_lines = self.machine.execute(
+            f"iw {iface} info | grep ssid || true"
+        ).strip().split('\n')
+        ssids = [line.removeprefix("ssid").strip() for line in ssid_lines if line]
+        return ssids
+
+    def _wait_for_ssids(self, iface, ssids):
+        wait(lambda: set(self.get_ssids(iface)) == set(ssids))
+
+    def check_ssids(self, iface, ssids):
+        assert set(self.get_ssids(iface)) == set(ssids)
