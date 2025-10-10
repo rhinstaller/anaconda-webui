2e46da2aebed96575d79d4410e7c4921c5d926a9
Adam Williamson <awilliam@redhat.com>
2025-10-09 17:27:06 -0700

    components/localization: Only offer ASCII-capable keyboard layouts
    As discussed in the bug report, it is dangerous to allow the
    user to configure a single, non-ASCII capable keyboard layout.
    This makes it almost impossible to set a usable encryption
    passphrase, or create usernames and passwords that use ASCII
    characters (which is common practice to avoid bugs in things that
    don't expect non-ASCII characters there).
    
    The correct fix would be for webUI to handle switched layout
    configurations correctly, but as a quick minimal fix, we can
    simply not offer layouts that do not support ASCII input. Note
    this requires an anaconda with
    https://github.com/rhinstaller/anaconda/pull/6692 in it.
    
    Resolves: rhbz#2402430
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- src/components/localization/Keyboard.jsx
+++ src/components/localization/Keyboard.jsx
@@ -42,6 +42,7 @@ const buildMenuItem = (keyboard) => {
     const {
         description,
         "is-common": isCommon,
+        "supports-ascii": supportsAscii,
         "layout-id": layoutId,
     } = keyboard;
 
@@ -72,6 +73,9 @@ const buildMenuItem = (keyboard) => {
 const buildMenuGroup = (keyboards, showCommon) => ({
     id: SCREEN_ID + "-keyboard-group-" + (showCommon ? "common" : "other") + "-keyboards",
     itemChildren: keyboards
+            // only offer layouts that support ASCII input, until we properly
+            // handle switched layout configurations
+            .filter(keyboard => keyboard["supports-ascii"]?.v === true)
             .filter(keyboard => keyboard["is-common"].v === showCommon)
             .map(keyboard => buildMenuItem(keyboard)),
     itemLabel: showCommon ? _("Suggested keyboards") : _("Other keyboards"),
