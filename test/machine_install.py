8d810eb4a3c89449ae7e25fa86d2adfdd2877873
Radek Vykydal <rvykydal@redhat.com>
2026-01-20 15:17:16 +0100

    Add support for additional boot options for tests
    Resolves: INSTALLER-4518

--- test/machine_install.py
+++ test/machine_install.py
@@ -142,6 +142,8 @@ class VirtInstallMachine(VirtMachine):
         selinux = "inst.noselinux " if os.environ.get("TEST_PAYLOAD", "liveimg").lower() == "dnf" else ""
 
         boot_arg = "--boot uefi " if self.is_efi else ""
+        extra_boot_args = os.environ.get("TEST_EXTRA_BOOT_ARGS", "")
+        extra_boot_args_option = f"--extra-args '{extra_boot_args}' " if extra_boot_args else ""
         try:
             self._execute(
                 "virt-install "
@@ -163,6 +165,7 @@ class VirtInstallMachine(VirtMachine):
                 f"hostfwd=tcp:{self.web_address}:{self.web_port}-:80 "
                 "-device virtio-net-pci,netdev=hostnet0,id=net0,addr=0x16' "
                 f"--extra-args '{extra_args}' "
+                f"{extra_boot_args_option}"
                 f"--disk=none "
                 f"--location {location} &"
             )
