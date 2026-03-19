rhinstaller/anaconda-webui

commit edfb91ee66fcba50f1a88b46f061def20fe3b963

components: keyboard: run Gnome keyboard settings only on Gnome variant

src/components/AnacondaWizard.jsx
@@ -26,7 +26,7 @@ import {
     WizardStep,
 } from "@patternfly/react-core";
 
-import { FooterContext, StorageContext, SystemTypeContext, UserInterfaceContext } from "../contexts/Common.jsx";
+import { FooterContext, StorageContext, UserInterfaceContext } from "../contexts/Common.jsx";
 
 import { AnacondaPage } from "./AnacondaPage.jsx";
 import { AnacondaWizardFooter } from "./AnacondaWizardFooter.jsx";
@@ -40,7 +40,7 @@ export const AnacondaWizard = ({ currentStepId, dispatch, isFetching, onCritFail
     // are failing the validation
     const [isFormValid, setIsFormValid] = useState(false);
     const { storageScenarioId } = useContext(StorageContext);
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const isBootIso = useContext(StorageContext).systemType === "BOOT_ISO";
     const userInterfaceConfig = useContext(UserInterfaceContext);
     const { path } = usePageLocation();
 

src/components/AnacondaWizardFooter.jsx
@@ -47,7 +47,7 @@ export const AnacondaWizardFooter = ({
     const [quitWaitsConfirmation, setQuitWaitsConfirmation] = useState(false);
     const { activeStep, goToNextStep, goToPrevStep, steps } = useWizardContext();
     const isFirstScreen = activeStep.id === steps.find(step => !step.isHidden).id;
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const isBootIso = useContext(SystemTypeContext).systemType === "BOOT_ISO";
     const {
         isFormDisabled,
         isFormValid,
@@ -127,7 +127,7 @@ export const AnacondaWizardFooter = ({
 };
 
 export const QuitInstallationConfirmModal = ({ exitGui, setQuitWaitsConfirmation }) => {
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const isBootIso = useContext(SystemTypeContext).systemType === "BOOT_ISO";
 
     return (
         <Modal

src/components/Error.jsx
@@ -104,7 +104,7 @@ export const BZReportModal = ({
 }) => {
     const [logContent, setLogContent] = useState();
     const [preparingReport, setPreparingReport] = useState(false);
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const isBootIso = useContext(SystemTypeContext).systemType === "BOOT_ISO";
 
     useEffect(() => {
         cockpit.spawn(["journalctl", "-a"])
@@ -241,7 +241,7 @@ const quitButton = (isBootIso) => {
 
 const CriticalError = ({ exception, isNetworkConnected, reportLinkURL }) => {
     const isConnected = isNetworkConnected;
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const isBootIso = useContext(SystemTypeContext).systemType === "BOOT_ISO";
     const context = exception.backendException?.contextData?.context || exception.frontendException?.contextData?.context;
     const description = context
         ? cockpit.format(_("The installer cannot continue due to a critical error: $0"), _(context))

src/components/HeaderKebab.jsx
@@ -77,7 +77,7 @@ const ProductName = () => {
 };
 
 const AnacondaAboutModal = ({ isModalOpen, setIsAboutModalOpen }) => {
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const isBootIso = useContext(SystemTypeContext).systemType === "BOOT_ISO";
     const toggleModal = () => {
         setIsAboutModalOpen(!isModalOpen);
     };

src/components/installation/Feedback.jsx
@@ -31,7 +31,7 @@ import "./Feedback.scss";
 const _ = cockpit.gettext;
 
 export const Feedback = () => {
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const isBootIso = useContext(SystemTypeContext).systemType === "BOOT_ISO";
 
     return (
         <Flex className="feedback-section">

src/components/installation/InstallationProgress.jsx
@@ -62,7 +62,7 @@ const InstallationProgress = ({ onCritFail }) => {
     const [steps, setSteps] = useState();
     const [currentProgressStep, setCurrentProgressStep] = useState(0);
     const refStatusMessage = useRef("");
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const isBootIso = useContext(SystemTypeContext).systemType === "BOOT_ISO";
     const osRelease = useContext(OsReleaseContext);
 
     useEffect(() => {

src/components/localization/InstallationLanguage.jsx
@@ -296,7 +296,8 @@ class LanguageSelector extends React.Component {
 
 const InstallationLanguage = ({ setIsFormValid, setStepNotification }) => {
     const { commonLocales, keyboardLayouts, language, languages } = useContext(LanguageContext);
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
+    const { desktopVariant } = useContext(SystemTypeContext);
+    const isGnome = desktopVariant === "GNOME";
 
     useEffect(() => {
         setIsFormValid(language !== "");
@@ -322,12 +323,13 @@ const InstallationLanguage = ({ setIsFormValid, setStepNotification }) => {
 
                 {keyboardLayouts.length > 0 && (
                     <FormGroup
-                      className={isBootIso ? "anaconda-screen-selectors-container" : ""}
+                      className={!isGnome ? "anaconda-screen-selectors-container" : ""}
                       fieldId={`${SCREEN_ID}-keyboard-layouts`}
                       label={_("Keyboard")}
                     >
                         <Keyboard
                           idPrefix={SCREEN_ID}
+                          isGnome={isGnome}
                           setIsFormValid={setIsFormValid}
                         />
                     </FormGroup>

src/components/localization/Keyboard.jsx
@@ -18,14 +18,21 @@
 import cockpit from "cockpit";
 
 import React, { useContext, useEffect, useState } from "react";
-import { Alert, Button, Content, Flex, FormSelect, FormSelectOption } from "@patternfly/react-core";
+import {
+    Alert,
+    Button,
+    Content,
+    Flex,
+    FormSelect,
+    FormSelectOption,
+} from "@patternfly/react-core";
 
 import {
     getKeyboardConfiguration,
     setCompositorLayouts,
 } from "../../apis/localization.js";
 
-import { LanguageContext, SystemTypeContext } from "../../contexts/Common.jsx";
+import { LanguageContext } from "../../contexts/Common.jsx";
 
 const _ = cockpit.gettext;
 
@@ -134,12 +141,10 @@ export const KeyboardGnome = ({ setIsFormValid }) => {
     );
 };
 
-export const Keyboard = ({ idPrefix, setIsFormValid }) => {
-    const isBootIso = useContext(SystemTypeContext) === "BOOT_ISO";
-
-    return (
-        isBootIso
-            ? <KeyboardSelector idPrefix={idPrefix} />
-            : <KeyboardGnome setIsFormValid={setIsFormValid} />
-    );
+export const Keyboard = ({ idPrefix, isGnome, setIsFormValid }) => {
+    if (isGnome) {
+        return <KeyboardGnome setIsFormValid={setIsFormValid} />;
+    } else {
+        return <KeyboardNonGnome idPrefix={idPrefix} />;
+    }
 };

commit 94e1fb5422bf98f5a6bf6a03cfcbc76348f76a80

Fix a typo in isBootIso setting in AnacondaWizard
Introduced by
commit edfb91ee66fcba50f1a88b46f061def20fe3b963

src/components/AnacondaWizard.jsx
@@ -10,7 +10,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { PageSection, PageSectionTypes } from "@patternfly/react-core/dist/esm/components/Page/index.js";
 import { Wizard, WizardStep } from "@patternfly/react-core/dist/esm/components/Wizard/index.js";
 
-import { FooterContext, PayloadContext, StorageContext, UserInterfaceContext } from "../contexts/Common.jsx";
+import { FooterContext, PayloadContext, StorageContext, SystemTypeContext, UserInterfaceContext } from "../contexts/Common.jsx";
 
 import { AnacondaPage } from "./AnacondaPage.jsx";
 import { AnacondaWizardFooter } from "./AnacondaWizardFooter.jsx";
@@ -24,7 +24,7 @@ export const AnacondaWizard = ({ currentStepId, dispatch, isFetching, onCritFail
     // are failing the validation
     const [isFormValid, setIsFormValid] = useState(false);
     const { storageScenarioId } = useContext(StorageContext);
-    const isBootIso = useContext(StorageContext).systemType === "BOOT_ISO";
+    const isBootIso = useContext(SystemTypeContext).systemType === "BOOT_ISO";
     const payloadType = useContext(PayloadContext).type;
     const userInterfaceConfig = useContext(UserInterfaceContext);
     const { path } = usePageLocation();